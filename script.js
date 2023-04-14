fb3a6ef201fc5e30ffdfe1ab4b4dc0eaa5ee491e
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-13 18:21:51 -0700

    using async/await method instead of .then() method

--- script.js
+++ script.js
@@ -6,17 +6,34 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-// fetch request - using below website API
-function generateJoke() {
+//* Async away method of fetch request.
+// USING ASYNC/AWAIT
+async function generateJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
     },
   }
 
-  fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke
-    })
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
 }
+
+//* USING .then()
+// fetch request - using below website API - This section is an alternative to the async request method which is preferable
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
