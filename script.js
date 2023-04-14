30fe6d43812d4fe14864db5843e2578c26eedffb
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-13 18:11:44 -0700

    added api request to js file

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// variables
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
 
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// fetch request - using below website API
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke
+    })
+}
